46d63c4ed6a28e7ee9a4c3b37b5698296c22584b
radubobe <46399340+radubobe@users.noreply.github.com>
2022-05-11 22:36:34 +0300

    Added strokes by age plot

---
+++ Strokes by age.R
@@ -0,0 +1,13 @@
+library(tidyverse)
+library(ggplot2)
+df<-read_csv("final_dataset.csv")
+df %>%
+group_by(smoking_status) %>% 
+summarize(total_strokes=sum(stroke)) %>%
+ggplot(aes(x=smoking_status, y=total_strokes)) +
+geom_bar(stat="identity", fill="steelblue")+
+geom_text(aes(label=total_strokes), vjust=1.6, color="black", size=3)+
+ggtitle("Total stroke cases by age") +
+xlab("Smoking_status") + ylab("Total Strokes")+
+theme(plot.title = element_text(hjust = 0.5), panel.background = element_blank())
+
